a90c19283fb5a5a829601ab0c0f80409430e4772
sage <98625216+saegben@users.noreply.github.com>
2022-10-11 18:49:49 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {   
+    Out o = new Out();
+    Board b = new Board();
+    
+		o.msgB("Welcome to Battleship!\n");
+		o.msgB("Try to hit my three ships within 30 guesses.\n\n");
+
+    b.createShips();
+
+    b.showShips(false); // change to 'true' to get ship coordinates
+
+    // Handle game
+    boolean win = false;
+		int guesses = 30;
+    
+		while (true) { 
+			b.showBoard();
+			int hits = b.handleUserGuess();
+
+      --guesses;
+      o.msgW("Guesses: " + guesses + "\n");
+
+      if (guesses == 0) {
+        break;
+      } 
+      if (hits == 11) {
+        win = true;
+        break;
+      }
+		}
+
+    if (win == true) {
+      b.showBoard();
+      o.msgG("\n\n───────────────────────────────────────────────");
+      o.msgG("\n              You sunk my ships!!!");
+			o.msgG("\n             Thanks for playing!!!!");
+      o.msgG("\n───────────────────────────────────────────────");
+		} else {
+      b.showBoard();
+      o.msgY("\n\n───────────────────────────────────────────────");
+			o.msgY("\n                   GAME OVER");
+			o.msgY("\n             Thanks for playing!!!!");
+      o.msgY("\n───────────────────────────────────────────────");
+		}
+    
+	}
+}
---
+++ Out.java
@@ -0,0 +1,30 @@
+// Decorate messages
+class Out {
+	public void msgW (String w) {
+    System.out.print(TEXT_RESET + TEXT_WHITE + w + TEXT_RESET);
+  }
+  public void msgBk (String bla) {
+    System.out.print(TEXT_RESET + TEXT_BLACK + bla + TEXT_RESET);
+  }
+  public void msgB (String blu) {
+    System.out.print(TEXT_RESET + TEXT_BLUE + blu + TEXT_RESET);
+  }
+  public void msgR (String r) {
+    System.out.print(TEXT_RESET + TEXT_RED + r + TEXT_RESET);
+  }
+  public void msgY (String y) {
+    System.out.print(TEXT_RESET + TEXT_YELLOW + y + TEXT_RESET);
+  }
+  public void msgG (String g) {
+    System.out.print(TEXT_RESET + TEXT_GREEN + g + TEXT_RESET);
+  }
+  
+  // Text colors
+  public static final String TEXT_RESET = "\u001B[0m";{}
+  public static final String TEXT_WHITE = "\u001B[37m";{}
+  public static final String TEXT_BLACK = "\u001B[30m";{}
+  public static final String TEXT_BLUE = "\u001B[34m";{}
+  public static final String TEXT_RED = "\u001B[31m";{}
+  public static final String TEXT_YELLOW = "\u001B[93m";{}
+  public static final String TEXT_GREEN = "\u001B[32m";{}
+}
