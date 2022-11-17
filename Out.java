fd201e70e2b25d20cd1b61c525342309cf95e409
sage <98625216+saegben@users.noreply.github.com>
2022-11-17 13:31:34 -0600

    Code cleanup

--- Out.java
+++ Out.java
@@ -1,6 +1,5 @@
-// Decorate messages
 class Out {
-  
+
   // Preset messages
   public void msgErr() { System.out.println(TEXT_RESET + RED + BLACK_BR + "\033[3m[ERROR] Something went wrong...\033[0m" + TEXT_RESET); }
   public void msgInv (String msg) { System.out.println(TEXT_RESET + BLACK_BR + RED + "\033[3m[UH OH!] \033[0m" + BLACK_BR + PINK + msg + TEXT_RESET); }
@@ -18,7 +17,7 @@ class Out {
   public void msgSky (String msg) { System.out.print(TEXT_RESET + SKY + msg + TEXT_RESET); }
   public void msgMag (String msg) { System.out.print(TEXT_RESET + MAGENTA + msg + TEXT_RESET); }
   public void msgCya (String msg) { System.out.print(TEXT_RESET + CYAN + msg + TEXT_RESET); }
-  
+
   public static final String BLACK = "\u001B[30m";{}
   public static final String RED = "\u001B[31m";{}
   public static final String GREEN = "\u001B[32m";{}
