ecc54b5ee474ec11b7e72751926557ad91c5aac4
sage <98625216+saegben@users.noreply.github.com>
2022-11-17 10:19:25 -0600

    More colors

--- UserHandling.java
+++ UserHandling.java
@@ -5,7 +5,7 @@ class UserHandling {
   Out o = new Out();
   Scanner getInput = new Scanner(System.in);
   
-  // Text Colors
+  // Text colors
   public static final String TEXT_RESET = "\u001B[0m";{}
 
   // Create logs
@@ -16,19 +16,19 @@ class UserHandling {
       // Clear logs & refresh
       if (file.exists() && file.isFile()) { file.delete(); }
       if (!file.createNewFile()) {
-        o.msgR("[ERROR] AN INTERRUPTION OCCURED");
-        o.msgR("Unable to create Logs file.\n");
+        o.msgErr();
+        o.msgInv("Unable to create 'Logs' file.");
       }
     } catch (IOException e) {
-      o.msgR("[ERROR] AN INTERRUPTION OCCURED");
-      o.msgR("Unable to read Logs file.\n");
+      o.msgErr();
+      o.msgInv("Unable to read 'Logs' file.");
     }
   }
 
   // Check user input
   public boolean checkInput(String row, int column) {
     String newInput = row + column;
-    o.msgB("\n\nYou entered: " + TEXT_RESET + newInput + "\n");
+    o.msgCya("\n\nYou entered: " + TEXT_RESET + newInput + "\n\n");
 
     // Cross-check with logs
     try (BufferedReader br = new BufferedReader(new FileReader("Logs.txt"));
@@ -52,13 +52,12 @@ class UserHandling {
         bw.newLine();
         return false;
       } else {
-        o.msgR("[ERROR] PROCESS INTERRUPTED\n");
-        o.msgR("Received duplicate position.\n");
-        o.msgB("You've already guessed that position... Try something new. Don't waste your guesses!\n");
+        o.msgInv("Received duplicate position.");
+        o.msgBlu("You've already guessed that position... Try something new. Don't waste your guesses!\n\n");
       }
     } catch (IOException e) {
-      o.msgR("[ERROR] AN INTERRUPTION OCCURED");
-      o.msgR("Unable to update Logs file.\n");
+      o.msgErr();
+      o.msgInv("Unable to update 'Logs' file.");
     }
     return true;
   }
@@ -70,21 +69,19 @@ class UserHandling {
     while (true) {
       try {
         // Get input
-        o.msgB("Which Row? (A/B/C/D/E/F/G/H/I/J/K/L)\n");
+        o.msgSky("Which Row? (A/B/C/D/E/F/G/H/I/J/K/L)\n");
         row = getInput.nextLine().substring(0, 1);
         if (row.matches("[a-zA-Z]")) { row = row.toUpperCase(); }
         // Check for match
         if (row.matches("[a-lA-L]")) {
           return row;
         } else {
-          o.msgR("[ERROR] INVALID INPUT\n");
-          o.msgR("Received non [A-L] row input.\n");
-          o.msgB("Rows must be between [A-L]... Try again.\n");
+          o.msgInv("Received non [A-L] row input.");
+          o.msgBlu("Rows must be between [A-L]... Try again.\n\n");
         }
       } catch (InputMismatchException e) {
-        o.msgR("[ERROR] INVALID INPUT\n");
-        o.msgR("Received inoperative input.\n");
-        o.msgB("Rows must be a letter between [A-L]... Try again.\n");
+        o.msgInv("Received inoperative input.");
+        o.msgBlu("Rows must be a letter between [A-L]... Try again.\n\n");
       }
     }
   }
@@ -100,20 +97,18 @@ class UserHandling {
     while (true) {
       try {
         // Get input
-        o.msgB("Which Column? (1/2/3/4/5/6/7/8/9/10/11/12/13/14/15)\n");
+        o.msgSky("Which Column? (1/2/3/4/5/6/7/8/9/10/11/12/13/14/15)\n");
         column = Integer.parseInt(getInput.nextLine().trim());
         // Check for match
         if (column >= 1 && column <= 15) {
           return column;
         } else {
-          o.msgR("[ERROR] INVALID INPUT\n");
-          o.msgR("Received non [1-15] column input.\n");
-          o.msgB("Columns must be between [1-15]... Try again.\n");
+          o.msgInv("Received non [1-15] column input.");
+          o.msgBlu("Columns must be between [1-15]... Try again.\n\n");
         }
       } catch (NumberFormatException e) {
-        o.msgR("[ERROR] INVALID INPUT\n");
-        o.msgR("Received inoperative input.\n");
-        o.msgB("Columns must be an integer between [1-15]... Try again.\n");
+        o.msgInv("Received inoperative input.");
+        o.msgBlu("Columns must be an integer between [1-15]... Try again.\n\n");
       }
     }
   }
@@ -124,29 +119,25 @@ class UserHandling {
   
   // Handle guesses
   public int handleGuesses(int guesses) {
-    // 30-16
-    if (guesses >= 16) {
-      o.msgB("\nGuesses: " + TEXT_RESET + guesses + "\n");
+    if (guesses >= 16) { // 30-16
+      o.msgBlu("Guesses: " + TEXT_RESET + guesses + "\n");
 
-    // 15-11
-    } else if (guesses >= 11) {
-      o.msgY("\nGuesses: " + TEXT_RESET + guesses + "\n");
+    } else if (guesses >= 11) { // 15-11
+      o.msgPur("Guesses: " + TEXT_RESET + guesses + "\n");
 
-    // 10-6
-    } else if (guesses >= 6) {
-      o.msgM("\nGuesses: " + TEXT_RESET + guesses + "\n");
+    } else if (guesses >= 6) { // 10-6
+      o.msgMag("Guesses: " + TEXT_RESET + guesses + "\n");
 
-    // 5-0
-    } else {
-      o.msgR("\nGuesses: " + TEXT_RESET + guesses + "\n");
+    } else { // 5-0
+      o.msgRed("Guesses: " + TEXT_RESET + guesses + "\n");
     }
 
-    // Warnings
+    // Warning
     if (guesses == 5) {
-      o.msgY("\nUh oh... You're running low on guesses... Choose your next move wisely.\n");
+      o.msgYel("\nUh oh... You're running low on guesses... Choose your next moves wisely.\n\n");
     }
 
     return guesses;
   }
-  
+
 }
