175c0e3725dde0b0897d0e503ef5110ca666d833
sage <98625216+saegben@users.noreply.github.com>
2022-11-17 08:57:53 -0600

    More input handling

--- Main.java
+++ Main.java
@@ -4,32 +4,43 @@ public class Main {
 	public static void main(String[] args) {   
     Out o = new Out();
     Board b = new Board();
+    UserHandling u = new UserHandling();
     
 		o.msgB("Welcome to Battleship!\n");
 		o.msgB("Try to hit my three ships within 30 guesses.\n\n");
 
+    // Setup
     b.createShips();
-
-    b.showShips(false); // change to 'true' to get ship coordinates
+    b.showShips(true); // change to 'true' to get ship coordinates
+    u.handleLogs();
 
     // Handle game
     boolean win = false;
 		int guesses = 30;
     
 		while (true) { 
+      u.handleGuesses(guesses);
 			b.showBoard();
-			int hits = b.handleUserGuess();
 
-      --guesses;
-      o.msgW("Guesses: " + guesses + "\n");
+      // Ask for user input until valid response      
+      boolean askUser = true;
+      while (askUser) {
+        if ((u.checkInput(u.promptRowInput(), u.promptColumnInput())) == false) {
+          askUser = false;
+          --guesses;
+        }
+      }
 
-      if (guesses == 0) {
-        break;
-      } 
+			int hits = b.handleMove(u.getRowInput(), u.getColumnInput());
+
       if (hits == 11) {
         win = true;
         break;
       }
+      else if (guesses == 0) {
+        break;
+      } 
+
 		}
 
     if (win == true) {
